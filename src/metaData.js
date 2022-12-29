8b86d2774fdcd966b7b798fbd218c6e537edd592
sarozpradhan64 <sarozpradhan64@gmail.com>
2022-12-29 23:50:20 +0545

    user meta data

--- src/metaData.js
+++ src/metaData.js
@@ -1,3 +1,16 @@
-const metas = { google_analytics_id: "G-1618WEC7X4" };
+const metas = {
+  google_analytics_id: "G-1618WEC7X4",
+  user: {
+    name: "Saroj Pradhan",
+    whoAmI: "Web Developer",
+    about: `As a web developer, I am skilled in the development and maintenance of 
+    websites and web applications. I have experience working with frameworks such as 
+    Laravel and Django, which allow for the efficient creation of scalable and reliable 
+    web applications. I also have knowledge of React, a popular JavaScript library for 
+    building user interfaces. I am constantly learning and staying up-to-date with the 
+    latest technologies and best practices in web development, and I am able to apply my
+     knowledge to solve complex problems and build high-quality web solutions.`,
+  },
+};
 
 export default metas;
