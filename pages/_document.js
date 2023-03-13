7bb25c2ca853bd9801519f0fc8ca0506d5f9f2d5
Saroj Pradhan <sarozpradhan64@gmail.com>
2023-03-13 11:33:05 +0000

    jsx attribute fix in _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -17,7 +17,7 @@ class MyDocument extends Document {
         <Head>
           {/* {CssBaseline.flush()} */}
     <link rel="preconnect" href="https://fonts.googleapis.com"/>
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet"/> 
 
 
