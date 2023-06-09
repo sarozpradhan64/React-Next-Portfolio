c2b3ffdedcd3cf4f6907e618a825305f39013529
Saroj Pradhan <sarozpradhan64@gmail.com>
2023-06-09 10:04:03 +0000

    Meta title update

--- src/metaData.js
+++ src/metaData.js
@@ -23,7 +23,7 @@ const metas = {
     latest technologies and best practices in web development, and I am able to apply my
      knowledge to solve complex problems and build high-quality web solutions.`,
   },
-  title:'Saroj Pradhan',
+  title:'Saroj Pradhan - Django, Laravel developer in Nepal',
   description: "Looking for a skilled web developer with expertise in Laravel, Django, React, and Node.js? Check out my portfolio website! As a freelancer, I've developed custom web applications and websites that meet the unique needs of my clients. Explore my portfolio and let's work together to bring your ideas to life!",
    imageSrc: `${getGithubLink() && getGithubLink()+'.png'}`,
    keywords:'web developer, software developer, freelancer, nepal, freelancer in kathmandu, web developer in kathmandu'
