13a556fff95f2e788e057f7e1ccae6ae225b3ddc
sarozpradhan64 <sarozpradhan64@gmail.com>
2024-05-12 00:50:31 +0545

    Contact form and email js setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ["admin.blazecodes.com", 'blazecodes.com', "system.pradhansaroj.com.np", 'wwww.blazecodes.com', "127.0.0.1", "images.unsplash.com"],
-      },
+  images: {
+    domains: [
+      "admin.blazecodes.com",
+      "blazecodes.com",
+      "system.pradhansaroj.com.np",
+      "wwww.blazecodes.com",
+      "127.0.0.1",
+      "images.unsplash.com",
+    ],
+  },
+
+  env: {
+    EMAIL_JS_SERVICE_ID: process.env.EMAIL_JS_SERVICE_ID,
+    EMAIL_JS_PUBLIC_KEY: process.env.EMAIL_JS_PUBLIC_KEY,
+    EMAIL_JS_TEMPLATE_ID: process.env.EMAIL_JS_TEMPLATE_ID,
+  },
 };
 
 export default nextConfig;
--- src/components/HighlightedProject.js
+++ src/components/HighlightedProject.js
@@ -17,7 +17,7 @@ function HighlightedProject() {
       <h2 className="mb-3 text-2xl font-medium text-white">
         Highlighted Projects
       </h2>
-      <span class="h-[2px] bg-secondary w-20 mb-8 inline-block"></span>
+      <span className="h-[2px] bg-secondary w-20 mb-8 inline-block"></span>
       <p className="text-justify">
         Amongst many of my works and project that i put my footprint in, here
         are some the major listings:
