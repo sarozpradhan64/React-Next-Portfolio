20770f7a7055e21681cdf2327e5e104f9668bca2
sarozpradhan64 <sarozpradhan64@gmail.com>
2024-07-04 21:28:58 +0545

    Sticky navigation bar

--- src/components/layouts/Navigation.js
+++ src/components/layouts/Navigation.js
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import data from "@/data/data";
 
@@ -8,33 +8,67 @@ const Navigation = () => {
   const links = data.routes;
   const pathname = usePathname();
 
+  const [isSticky, setSticky] = useState(false);
+
   useEffect(() => {
-    window.scrollTo({
-      top: 0,
-    });
+    // Scroll to top when the component mounts
+    window.scrollTo({ top: 0 });
+
+    const handleScroll = () => {
+      if (window.scrollY > 850) {
+        setSticky(true);
+      } else {
+        setSticky(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // remove event listener when the component is unmounted
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
 
   return (
-    <div>
-      {/* nav */}
-      <div className="md:block hidden">
-        {links.map((link, index) => (
-          <Link
-            href={link.href}
-            key={index}
-            className={`${
-              link.title === "works"
-                ? pathname === link.href || pathname.includes("/works/")
-                  ? "active-nav-item"
-                  : ""
-                : pathname === link.href
-                ? "active-nav-item"
-                : ""
-            } nav-item text-white px-3 py-2  mx-2`}
-          >
-            {link.title.toUpperCase()}
-          </Link>
-        ))}
+    <>
+      <div
+        className={`${
+          isSticky ? "sticky shadow-sm" : ""
+        }  md:block hidden navigation_wrapper`}
+      >
+        <div className="container">
+          <nav className=" md:py-2 px-4 md:px-5 flex justify-between items-center  w-full">
+            <div>
+              <Link href={"/"}>
+                <h2 className="logo-title text-secondary">Srz</h2>
+              </Link>
+            </div>
+
+            <div>
+              {/* nav */}
+              <div className="">
+                {links.map((link, index) => (
+                  <Link
+                    href={link.href}
+                    key={index}
+                    className={`${
+                      link.title === "works"
+                        ? pathname === link.href || pathname.includes("/works/")
+                          ? "active-nav-item"
+                          : ""
+                        : pathname === link.href
+                        ? "active-nav-item"
+                        : ""
+                    } nav-item text-white px-3 py-2  mx-2`}
+                  >
+                    {link.title.toUpperCase()}
+                  </Link>
+                ))}
+              </div>
+            </div>
+          </nav>
+        </div>
       </div>
 
       {/* mobile nav */}
@@ -62,7 +96,7 @@ const Navigation = () => {
         ))}
       </div>
       {/* mobile nav end  */}
-    </div>
+    </>
   );
 };
 
