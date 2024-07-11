ba7a77d746f2851d8043d345591a75f2f0cd2624
sarozpradhan64 <sarozpradhan64@gmail.com>
2024-07-11 23:09:51 +0545

    Fix case sensitivity issues in layouts folder and files

--- src/utils/getWorks.js
+++
@@ -1,7 +0,0 @@
-export default async function getWorks() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/works`, {
-    next: { revalidate: 3600 },
-  });
-  if (!res.ok) throw new Error("Failed to fetch works");
-  return res.json();
-}
