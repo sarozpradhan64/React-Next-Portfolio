858d0d2d067a889968b3aff38e5802ff9c89a8a3
sarozpradhan64 <sarozpradhan64@gmail.com>
2024-07-19 00:36:02 +0545

    Hero section refactored and optimized

--- src/app/page.js
+++ src/app/page.js
@@ -20,50 +20,49 @@ export default async function Home() {
 
   return (
     <FrontendLayout>
-      <div className="container" id="home">
-        <div className="home-cover">
-          <div className="absolute lg:w-[500px] lg:h-[500px] 2xl:right-[200px] lg:right-[100px] lg:top-[50%] -translate-y-[50%] lg:block hidden">
+      <div className="flex items-center justify-between h-[65vh]">
+        <div>
+          <h3 className="text-3xl text-primary mb-3 font-bold">
+            Hi 👋, I&apos;m
+          </h3>
+          <h1 className="text-6xl mb-8 text-white">{metas.user.name}</h1>
+          <h2 className="text-3xl text-white inline text-secondary">
+            {metas.user.whoAmI}
+          </h2>
+          <div className="text-light hidden">
+            Web Designer, Web Developer, Front End Developer, Apps Designer,
+            Apps Developer
+          </div>
+          <div className="flex  md:flex-row flex-col md:items-center items-start md:mt-8 mt-6">
+            <Link href="/contact" className="me-6 md:mb-0 mb-2">
+              <Button>Work with me</Button>
+            </Link>
+
+            <Link href="/works">
+              <Button color="secondary">Explore My Works</Button>
+            </Link>
+          </div>
+        </div>
+
+        <div className=" lg:block hidden">
+          <div className=" relative xxl:w-[480px] xxl:h-[480px] w-[380px] h-[380px]">
             <Image
               src={metas.photoPath}
               fill
               alt={`${metas.user.name} Portfolio Photo`}
-              style={{ objectFit: "cover" }}
+              className="object-cover"
             />
-          </div>
-        </div>
-        <div className="home-container">
-          <div className="grid lg:grid-cols-2 g-5 items-center">
-            <div className=" py-6 pb-0 lg:pt-0">
-              <h3 className="text-3xl text-primary mb-3 font-bold">
-                Hi 👋, I&apos;m
-              </h3>
-              <h1 className="text-6xl mb-8 text-white">{metas.user.name}</h1>
-              <h2 className="text-3xl text-white inline text-secondary">
-                {metas.user.whoAmI}
-              </h2>
-              <div className="text-light hidden">
-                Web Designer, Web Developer, Front End Developer, Apps Designer,
-                Apps Developer
-              </div>
-              <div className="flex  md:flex-row flex-col md:items-center items-start md:mt-8 mt-6">
-                <Link href="/contact" className="me-6 md:mb-0 mb-2">
-                  <Button>Work with me</Button>
-                </Link>
-
-                <Link href="/works">
-                  <Button color="secondary">Explore My Works</Button>
-                </Link>
-              </div>
-            </div>
           </div>
         </div>
       </div>
 
-      <div className="grid md:grid-cols-2 gap-5">
+      <div className="mt-16 grid md:grid-cols-2 gap-5">
         <div>
           <SectionTitle title={"Know about me !"} />
 
-          <p className="mb-4 tracking-wide leading-relaxed">{metas.user.about}</p>
+          <p className="mb-4 tracking-wide leading-relaxed">
+            {metas.user.about}
+          </p>
           <p className="mb-3">
             <i className="far fa-check-circle text-primary me-3"></i>
             Demand Analysis
