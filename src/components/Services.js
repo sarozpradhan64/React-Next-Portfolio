c67023f4b62a0260caea7a493f231a6a5ee25814
sarozpradhan64 <sarozpradhan64@gmail.com>
2024-07-21 22:11:21 +0545

    services select none for description

--- src/components/Services.js
+++ src/components/Services.js
@@ -28,17 +28,13 @@ export default function Services() {
               >
                 <div className="bg-[#1C1F26] sm:p-4 p-2 flex flex-row  md:gap-4 gap-2  rounded-md border border-transparent hover:border-gray-600">
                   <div className="md:w-1/5 sm:h-auto h-[60px] w-[30%] relative">
-                    <Image
-                      src={service.thumbnail}
-                      fill
-                      alt={service.title}
-                    />
+                    <Image src={service.thumbnail} fill alt={service.title} />
                   </div>
                   <div className="md:w-4/5 w-[70%]">
                     <h5 className="sm:text-md text-sm text-white sm:mb-2 mb-1">
                       {service.title}
                     </h5>
-                    <p className="text-sm md:leading-6">
+                    <p className="text-sm md:leading-6 select-none">
                       {service.description}
                     </p>
                   </div>
