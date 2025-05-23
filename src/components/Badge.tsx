441421ac6a443a577e1d46c4a9ad54be8da8d4af
Saroj Pradhan <54489044+sarozpradhan64@users.noreply.github.com>
2025-05-23 22:07:31 +0545

    Highlighted Work Card Update.

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -17,14 +17,14 @@ interface BadgeProps {
 
 const Badge: React.FC<BadgeProps> = ({ color = "gray", children }) => {
   const bgColorMap: Record<BadgeColor, string> = {
-    gray: "bg-gray-50 text-gray-600",
-    red: "bg-red-50 text-red-700",
-    yellow: "bg-yellow-50 text-yellow-800",
-    green: "bg-green-50 text-green-700",
-    blue: "bg-blue-50 text-blue-700",
-    indigo: "bg-indigo-50 text-indigo-700",
-    purple: "bg-purple-50 text-purple-700",
-    pink: "bg-pink-50 text-pink-700",
+    gray: "bg-gray-100 text-gray-600",
+    red: "bg-red-100 text-red-700",
+    yellow: "bg-yellow-100 text-yellow-800",
+    green: "bg-green-100 text-green-700",
+    blue: "bg-blue-100 text-blue-700",
+    indigo: "bg-indigo-100 text-indigo-700",
+    purple: "bg-purple-100 text-purple-700",
+    pink: "bg-pink-100 text-pink-700",
   };
 
   return (
---
+++ src/components/WorkCard.tsx
@@ -0,0 +1,54 @@
+import { Work } from "@/types/work";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Badge from "./Badge";
+
+const WorkCard = ({ work }: { work: Work }) => {
+  const colors: string[] = [
+    "red",
+    "yellow",
+    "green",
+    "blue",
+    "indigo",
+    "purple",
+    "pink",
+  ];
+
+  return (
+    <div
+      className="portfolio-img rounded-xl relative"
+      style={{ height: "250px" }}
+    >
+      <Image
+        className="img-fluid object-cover"
+        src={work.image}
+        alt={work.title}
+        fill
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+      />
+
+      <div className="portfolio-btn z-[100] rounded-xl absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-300">
+        <Link href={`/works/${work.slug}`}>
+          <h4 className="portfolio-hover-title text-white text-center hover:underline">
+            {work.title}
+          </h4>
+        </Link>
+      </div>
+      <div className="absolute z-50 bottom-2 left-2">
+        <h4 className="text-white mb-2">{work.title}</h4>
+        <div className="space-x-2">
+          {work.stacks.map((stack, index) => (
+            <Badge key={index} color={colors[index]}>
+              {stack}
+            </Badge>
+          ))}
+        </div>
+      </div>
+
+      <div className="absolute inset-0 z-10 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
+    </div>
+  );
+};
+
+export default WorkCard;
